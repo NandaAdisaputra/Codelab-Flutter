9bc19c1440c4890232351bd39d86ecea86640bb6
Nanda Adisaputra <nandaadisaputra18@gmail.com>
2023-11-30 23:59:18 +0700

    Add Image Screen ( URL Image )

--- lib/image_screen.dart
+++ lib/image_screen.dart
@@ -7,8 +7,19 @@ class ImageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("IMAGE SCREEN"),backgroundColor: Colors.amber,),
-      body: Center(
-        child: Image.asset('assets/images/image_flutter.png', width: 200, height: 200)),
+      body: Column(children: [
+      Center(child: Image.asset('assets/images/image_flutter.png', width: 200, height: 200)),
+
+      SizedBox(height: 16),
+
+      Center(child: Image( image: AssetImage('assets/images/image_flutter.png'))),
+
+      SizedBox(height: 16),
+
+      Center(
+      child: Image.network('https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/flutter%20logo_omSaocF.png'
+    , width: 200, height: 200)),
+    ])
     );
   }
 }
