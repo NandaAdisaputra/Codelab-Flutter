07cac5a9604757c13df6fc374ccd62376cbec9bf
Nanda Adisaputra <nandaadisaputra18@gmail.com>
2023-12-01 00:56:22 +0700

    Add Dropdown Button

--- lib/button_screen.dart
+++ lib/button_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-class ButtonScreen extends StatelessWidget {
+class ButtonScreen extends StatefulWidget {
   const ButtonScreen({super.key});
+  @override
+  State<ButtonScreen> createState() => _FirstScreenState();
+}
+class _FirstScreenState extends State<ButtonScreen> {
+  String? progdi;
 
   @override
   Widget build(BuildContext context) {
+    String? language;
     return Scaffold(
       appBar: AppBar(title: Text("BUTTON SCREEN"), backgroundColor: Colors.amber,),
       body: Column(
@@ -16,10 +22,37 @@ class ButtonScreen extends StatelessWidget {
               onPressed: (){}, child: Text("Simpan", style: TextStyle(color: Colors.white),)),
           OutlinedButton(onPressed: (){}, child: Text("Batal", style: TextStyle(color: Colors.blue),)),
           TextButton(onPressed: (){}, child: Text("Lihat Selengkapnya", style: TextStyle(color: Colors.blue),)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.visibility))
+          IconButton(onPressed: (){}, icon: Icon(Icons.visibility)),
+          DropdownButton<String>(
+             items: const <DropdownMenuItem<String>>[
+               DropdownMenuItem<String>(
+                 value: 'S1 Teknik Informatika',
+                 child: Text('S1 Teknik Informatika'),
+               ),
+               DropdownMenuItem<String>(
+                 value: 'S1 Sistem Informasi',
+                 child: Text('S1 Sistem Informasi'),
+               ),
+               DropdownMenuItem<String>(
+                 value: 'S1 Ilmu Komunikasi',
+                 child: Text('S1 Ilmu Komunikasi'),
+               ),
+               DropdownMenuItem<String>(
+                 value: 'S1 Pariwisata',
+                 child: Text('S1 Pariwisata'),
+               ),
+             ],
+             value: progdi,
+             hint: const Text('Pilih Program Studi'),
+             onChanged: (String? value) {
+               setState(() {
+                 progdi = value;
+               });
+               },
+           ),
         ],),
         
-        SizedBox(height: 150,),
+        SizedBox(height: 100,),
         Column(children: [
           Container(
             margin: EdgeInsets.all(8),
